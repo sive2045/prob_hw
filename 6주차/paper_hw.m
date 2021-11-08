1d5ea7973d4a54d6054818bb4c9b412c9b226fe5
sive2045 <lc9902130509@gmail.com>
2021-11-09 00:14:33 +0900

    mod : paper_hw.m 가우시안 곱 분포까지 함 To do: eta에 따른 분포의 변화 확인하기

--- 6주차/paper_hw.m
+++ 6주차/paper_hw.m
@@ -1,6 +1,7 @@
 %% init value
+clear; clc;
 sample = 1000000; interval = 0.01;
-x = [-10:interval:10]; eta = 0.001;
+x = [-10:interval:10]; eta = 0.01;
 
 %% generate normal distbution
 Nxa = normrnd(2,1, [1,sample]);
@@ -12,21 +13,30 @@ Nb = histcounts(Nxb, x);
 Nc = histcounts(Nxc, x);
 
 sumX = Na + Nb;
-sumX_pdf = sumX/sample*2/interval;
+sumX_pdf = sumX/sample/interval;
+sumX_pdf = sumX_pdf/2;
 
-prodX = Nc + eta*sumX;
-prodX_pdf = prodX/sample*3/interval;
+prodX = Na.*Nb;
+prodX_pdf = prodX/sample/interval;
+prodX_pdf = prodX_pdf/1000;
 
 Na_pdf = Na/sample/interval;
 Nb_pdf = Nb/sample/interval;
+Nc_pdf = Nc/sample/interval;
 
 Na_cdf = cumsum(Na_pdf)*interval;
+sumX_cdf = cumsum(sumX_pdf)*interval;
+prodX_cdf = cumsum(prodX_pdf)*interval;
 
-%% plot PDF
-figure(1); hold on;
-bar(x(1:length(x)-1), Na_pdf, 'r');
-bar(x(1:length(x)-1), Nb_pdf, 'g'); 
+%% plot Xa, Xb, Xc's PDF
+figure(1); hold on; 
+stairs(x(1:length(x)-1), Na_pdf, 'r');
+stairs(x(1:length(x)-1), Nb_pdf, 'g'); 
+stairs(x(1:length(x)-1), Nc_pdf, 'blue');
+title('Normal Distribution(1,000,000 sampling)')
+ylabel('PDF'); xlabel('x');
+legend('X_{a} ~ N(2,1)', 'X_{b} ~ N(4,1)', 'X_{c} ~ N(-2,1)', 'Location','northwest');
 hold off;
 
 figure(2);
-bar(x(1:length(x)-1), prodX_pdf);
+stairs(x(1:length(x)-1), prodX_pdf);
